6e9d963ba6adca3ba6e986a017292e16111b0171
Francisco JSP <32346571+franciscosousajsp@users.noreply.github.com>
2024-10-30 20:49:04 -0300

    Update operacoes.js
    Criação do operador de multiplicação

--- operacoes.js
+++ operacoes.js
@@ -11,8 +11,10 @@ var numero1 = entrada("Informe um número: ")
 var numero2 = entrada("Informe outro número: ")
 
 var soma = Number(numero1) + Number(numero2)
-
 var subtracao = Number(numero1) - Number(numero2)
+var multiplicacao = Number(numero1) * Number(numero2)
+
 
 console.log("Resultado da Soma é "+ soma)
-console.log("Resultado da Soma é "+ subtracao)
+console.log("Resultado da Subtracao é "+ subtracao)
+console.log("Resultado da Multplicacao é "+ multiplicacao)
